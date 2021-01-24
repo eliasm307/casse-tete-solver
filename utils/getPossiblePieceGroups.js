044f514093e2b82ae4b90955085409c4a444535c
Eli <13420359+eliasm307@users.noreply.github.com>
2021-01-25 00:47:48 +0100

    tsc config update

--- utils/getPossiblePieceGroups.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-/**
- * This function finds all possible ways to split the 6 pieces into 2 groups of 3 pieces
- * and makes sure groups are not repeated ie a group with pieces 1,2,3 is the same as a group with pieces 3,2,1
- */
-function getPossiblePieceGroups(pieces) { }
-module.exports = getPossiblePieceGroups;
