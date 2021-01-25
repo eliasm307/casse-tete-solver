5578ec6a7ecf75b5cea0f6da9dca6e8c639eaa38
Eli <13420359+eliasm307@users.noreply.github.com>
2021-01-25 17:15:11 +0100

    Updated testPieceGroups to map and updated tests

--- src/utils/getPossiblePieceGroups.ts
+++ src/utils/getPossiblePieceGroups.ts
@@ -1,17 +1,20 @@
 import combinations = require('combinations');
 
-import { Pieces } from '../tsc/types';
-
 /**
  * This function finds all possible ways to split the 6 pieces into 2 groups of 3 pieces
  * and makes sure groups are not repeated ie a group with pieces 1,2,3 is the same as a group with pieces 3,2,1
  */
-function getPossiblePieceGroups(pieces: Pieces): PieceGroups {
+function getPossiblePieceGroups(pieces: Pieces): PieceIdGroups {
 	const pieceIDs: number[] = [...pieces].map((piece, id) => id);
 
-	const pieceIDGroups: Array<number[]> = combinations(pieceIDs, 3, 3);
+	const combinationsWithIds: Array<[string, number[]]> = combinations(pieceIDs, 3, 3).map(pieceIDGroup => [
+		pieceIDGroup.toString(),
+		pieceIDGroup,
+	]);
 
-	console.log(__filename, { pieceIDs, pieceIDGroups });
+	const pieceIDGroups: PieceIdGroups = new Map<string, number[]>(combinationsWithIds);
+
+	console.log(__filename, { pieceIDs, combinationsWithIds, pieceIDGroups });
 
 	// return piece groups with actual piece objects
 	/*return pieceIDGroups.map( pieceIDGroup => {
