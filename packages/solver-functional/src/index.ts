442ed65a88fa091d56483ec36a48ddc858204118
Eli <13420359+eliasm307@users.noreply.github.com>
2024-01-21 22:02:30 +0000

    make functional run both implementations

--- packages/solver-functional/src/index.ts
+++ packages/solver-functional/src/index.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import {
   AVAILABLE_PIECES_ARRAY,
   CLIENT_GENERAL_SOLUTIONS_DIR,
@@ -5,16 +6,18 @@ import {
 } from "@casse-tete-solver/common/src/constants";
 import path from "path";
 import { saveJsonToFile } from "@casse-tete-solver/common/src/utils";
+import type { GeneralSolution } from "@casse-tete-solver/common/src/types";
 import { findSolutionsBfs, findSolutionsDfs } from "./findSolutions";
 
 async function main() {
-  const timerKey = "findSolutions";
-  console.time(timerKey);
-
-  const solutions = findSolutionsDfs({ availablePieces: AVAILABLE_PIECES_ARRAY });
-  console.log("solutions:", solutions.length);
-
-  console.timeEnd(timerKey);
+  let solutions: GeneralSolution[] = [];
+  [findSolutionsBfs, findSolutionsDfs].forEach((findSolutions) => {
+    const timerKey = findSolutions.name;
+    console.time(timerKey);
+    solutions = findSolutions({ availablePieces: AVAILABLE_PIECES_ARRAY });
+    console.log("solutions:", solutions.length);
+    console.timeEnd(timerKey);
+  });
 
   const outputDir = path.join(__dirname, "../output");
   await saveJsonToFile(solutions, path.join(outputDir, "solutions.json"));
